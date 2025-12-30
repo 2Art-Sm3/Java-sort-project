88621226a71f74a2b511f21c0eee14a171b40771
2Art-Sm3 <artem_smirnov_smirnov@gmail.com>
2025-12-30 22:15:56 +0500

    Начальная структура проекта

---
+++ src/ru/sortproject/model/Car.java
@@ -0,0 +1,20 @@
+package ru.sortproject.model;
+
+public class Car {
+    private int power;
+    private String model;
+    private int year;
+
+    private Car() {}
+
+    public static class Builder {
+        private final Car car = new Car();
+
+        public Builder setPower(int power) { car.power = power; return this; }
+        public Builder setModel(String model) { car.model = model; return this; }
+        public Builder setYear(int year) { car.year = year; return this; }
+
+        public Car build() { return car; }
+    }
+
+}
