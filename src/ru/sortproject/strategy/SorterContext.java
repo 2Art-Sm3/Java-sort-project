5ad63fa7f4ca15e9caebb747f514a4cc04a5a046
Павел <banakov564@gmail.com>
2026-01-10 16:55:40 +0300

    Доработка
    Поменял метод loadFromFile под наш формат toString
    Реализовал запись отсортированной коллекции в файл в режиме append, создав метод saveSortedToFile.
    Сделал тест и создал текстовый файл.

--- src/ru/sortproject/strategy/SorterContext.java
+++ src/ru/sortproject/strategy/SorterContext.java
@@ -1,7 +1,10 @@
 package ru.sortproject.strategy;
 
+import ru.sortproject.model.Car;
 import ru.sortproject.structure.CustomList;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Comparator;
 
 public class SorterContext<T> {
@@ -18,5 +21,16 @@ public class SorterContext<T> {
             return;
         }
         strategy.sort(list, comparator);
+    }
+
+    public void saveSortedToFile(CustomList<Car> list, String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            for (Car car : list) {
+                writer.write(car.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/ru/sortproject/test/TestLauncher4.java
+++ src/ru/sortproject/test/TestLauncher4.java
@@ -1,7 +1,40 @@
 package ru.sortproject.test;
 
+import ru.sortproject.model.Car;
+import ru.sortproject.strategy.BubbleSortStrategy;
+import ru.sortproject.structure.CustomList;
+import ru.sortproject.strategy.SorterContext;
+import ru.sortproject.util.CarComparator;
+import ru.sortproject.util.DataLoader;
+
+import java.io.File;
+
 public class TestLauncher4 {
     public static void main(String[] args) {
+
+        String directoryPath = "src/ru/sortproject/txtfiles"; // название папки внутри проекта
+        File dir = new File(directoryPath);
+        File outputFile = new File(dir, "sorted_cars.txt");
+        String fullPath = outputFile.getAbsolutePath();
+
+        CustomList<Car> cars = DataLoader.loadRandom(5); // метод loadRandom
+        SorterContext<Car> sorter = new SorterContext<>();
+        BubbleSortStrategy<Car> sortCarsBub = new BubbleSortStrategy<>();
+        CarComparator comparator = new CarComparator();
+
+        sorter.setStrategy(sortCarsBub);
+        sorter.executeSort(cars,comparator);
+        sorter.saveSortedToFile(cars,fullPath);     // Запись отсортированной коллекции в файл в режиме append
+
+        cars = DataLoader.loadManual();             // Ручной ввод коллекции
+        sorter.executeSort(cars,comparator);
+        sorter.saveSortedToFile(cars,fullPath);
+
+        cars = DataLoader.loadFromFile(fullPath);   //Загрузка коллекции из файла в формате toString
+        sorter.executeSort(cars,comparator);
+        sorter.saveSortedToFile(cars,fullPath);
+
+        System.out.println("Тест завершён!");
 
     }
 }
